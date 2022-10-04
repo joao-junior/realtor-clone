54fb14d3d61aa19c71c5ac720bfbc3d06bc93c80
joao-junior <joaoxdd2011@hotmail.com>
2022-10-04 08:04:53 -0500

    complete the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react'
 import {useLocation, useNavigate} from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 function Header() {
+    const [pageState, setPageState] = useState("Sign in")
     const location = useLocation()
     const navigate = useNavigate()
+    const auth = getAuth()
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setPageState("Profile")
+            } else {
+                setPageState("Sign in")
+            }
+        })
+    }, [auth])
 
     function pathMatchRoute(route) {
         if (route === location.pathname) {
@@ -20,7 +34,7 @@ function Header() {
                     <ul className="flex space-x-10">
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={() => navigate("/sign-in")}>Sign In</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-red-500"}`} onClick={() => navigate("/profile")}>{pageState}</li>
                     </ul>
                 </div>
             </header>
